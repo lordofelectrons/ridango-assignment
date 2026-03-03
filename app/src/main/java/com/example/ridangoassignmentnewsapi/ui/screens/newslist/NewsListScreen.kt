d0988640f83c5df6ef35920483a9eb61c98d5017
Artem Filipenko <artemnayamateria@gmail.com>
2026-03-03 21:48:35 +0200

    Add reload button

--- app/src/main/java/com/example/ridangoassignmentnewsapi/ui/screens/newslist/NewsListScreen.kt
+++ app/src/main/java/com/example/ridangoassignmentnewsapi/ui/screens/newslist/NewsListScreen.kt
@@ -14,7 +14,11 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -52,6 +56,14 @@ fun NewsListScreen(
                             text = "by Artem for Ridango",
                             style = MaterialTheme.typography.labelSmall,
                             color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { viewModel.loadArticles() }) {
+                        Icon(
+                            imageVector = Icons.Default.Refresh,
+                            contentDescription = "Reload"
                         )
                     }
                 }
